ae14960b9da6305873fd0276d422a4eb9593ea0e
tekater <120014627+tekater@users.noreply.github.com>
2023-01-16 18:33:48 +0900

    Update Box.java

--- src/Box.java
+++ src/Box.java
@@ -3,40 +3,19 @@ double height;
 double width;
 double length;
 double Volume;
-
-    public double getHeight() {
-        return height;
-    }
-
     public void setHeight(double height) {
         this.height = height;
     }
-
-    public double getWidth() {
-        return width;
-    }
-
     public void setWidth(double width) {
         this.width = width;
     }
-
-    public double getLength() {
-        return length;
-    }
-
     public void setLength(double length) {
         this.length = length;
     }
-
     public double getVolume() {
         Volume = height * width * length;
         return Volume;
     }
-
-    /*public void setVolume(double volume) {
-        Volume = volume;
-    }*/
-
     @Override
 public String toString () {
     return "Box{" +
